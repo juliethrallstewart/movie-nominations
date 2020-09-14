33dba88f7cde82fb4ddb9d347c62ed1d0c99c83c
Julie Thrall <juliethrallstewart@gmail.com>
2020-09-13 19:00:48 -0700

    removed unused hook

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useRef} from "react";
+import React, {useContext, useEffect} from "react";
 import NominationsContext from '../../contexts/NominationsContext'
 
 

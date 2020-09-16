c25acaea8cefdebafe1fcb6c53ce60862fd5de5b
Julie Thrall <juliethrallstewart@gmail.com>
2020-09-16 10:49:26 -0700

    ux improvement banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -9,11 +9,11 @@ const Banner = () => {
         <div className={counter > 3 && bannerStatus === true ? "banner-box" : "hidden"}>
             <div className="banner-content">
                 <div className="close-button-box">
-                    <button className="close-button" onClick={(() => {setBannerStatus(!bannerStatus); console.log("status", bannerStatus)})}>X</button>
+                    <button className="close-button" onClick={(() => setBannerStatus(!bannerStatus))}>X</button>
                 </div>
                 <div className="banner-message">
                     <h3 className="banner-font">You've reached your max 5 nominations!</h3> 
-                    <h3 className="banner-font">Remove a nomination to make a different selection.</h3>
+                    <p className="banner-font">Remove a nomination to make a different selection.</p>
                 </div>
             </div>
         </div>
